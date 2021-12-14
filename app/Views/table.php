a6c65790041b4e23f0b7b47e9eb7edce309dc8b6
Christos Karamolegkos <xristoskaramo@gmail.com>
2021-12-14 19:36:55 +0200

    Improve table layout

--- app/Views/table.php
+++ app/Views/table.php
@@ -7,35 +7,33 @@ if (isset($columns, $apiTarget)): ?>
 
     <main role="main" class="py-5">
         <div class="container">
-            <div class="row">
-                <div id="table"></div>
+            <div id="table"></div>
 
-                <script>
-                    let table = new Tabulator("#table", {
-                        index: "id",
-                        layout: "fitColumns",
-                        responsiveLayout: "hide",
-                        width: "100%",
-                        columns: [
-                            {title: "id", field: "id", visible: false},
-                            <?= $columns ?>
-                            {
-                                title: "<?= lang('Text.delete') ?>",
-                                formatter: "buttonCross",
-                                hozAlign: "center",
-                                cellClick: function (e, cell) {
-                                    cell.getRow().delete();
-                                }
+            <script>
+                let table = new Tabulator("#table", {
+                    index: "id",
+                    layout: "fitColumns",
+                    responsiveLayout: "hide",
+                    columns: [
+                        {title: "id", field: "id", visible: false},
+                        <?= $columns ?>
+                        {
+                            title: "<?= lang('Text.delete') ?>",
+                            formatter: "buttonCross",
+                            hozAlign: "center",
+                            cellClick: function (e, cell) {
+                                cell.getRow().delete();
                             },
-                        ],
-                        ajaxURL: "<?= $apiTarget ?>",
-                        ajaxResponse: function (url, params, response) {
-                            return response.data;
+                            width:140
                         },
-                    });
+                    ],
+                    ajaxURL: "<?= $apiTarget ?>",
+                    ajaxResponse: function (url, params, response) {
+                        return response.data;
+                    },
+                });
 
-                </script>
-            </div>
+            </script>
         </div>
     </main>
 
