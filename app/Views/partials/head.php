c6e214c0c8b62515f47cf91be625ff90564cf631
Christos Karamolegkos <xristoskaramo@gmail.com>
2021-12-14 19:33:48 +0200

    Update tabulator
    Update tabulator 4.9.3 -> 5.0.7

--- app/Views/partials/head.php
+++ app/Views/partials/head.php
@@ -19,8 +19,8 @@
     <?php
     if (isset($tabulator) && $tabulator) : ?>
         <!-- Tabulator CSS -->
-        <link href="https://unpkg.com/tabulator-tables@4.9.3/dist/css/tabulator.min.css" rel="stylesheet">
-        <script type="text/javascript" src="https://unpkg.com/tabulator-tables@4.9.3/dist/js/tabulator.min.js"></script>
+        <link href="https://unpkg.com/tabulator-tables@5.0.7/dist/css/tabulator.min.css" rel="stylesheet">
+        <script type="text/javascript" src="https://unpkg.com/tabulator-tables@5.0.7/dist/js/tabulator.min.js"></script>
     <?php
     endif; ?>
 
