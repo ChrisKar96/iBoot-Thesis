dafb59818faebe88eefd392c59d48c45562c9815
Christos Karamolegkos <xristoskaramo@gmail.com>
2021-12-15 23:10:20 +0200

    Add instructions in boot.php
    Add instructions in boot.php and move ipxe menu

--- app/Language/en/Text.php
+++ app/Language/en/Text.php
@@ -43,4 +43,6 @@ return [
     'reset_table'            => 'Reset Table',
     'save_table'             => 'Save Table',
     'confirm_delete'         => 'Do you really want to delete this item?',
+    'configure_your_dhcp'    => 'Please configure your DHCP server to point to this page.',
+    'add_to_your_dhcpd'      => 'Add the following code snippet to the dhcpd.conf file of your existing ISC installation.',
 ];
