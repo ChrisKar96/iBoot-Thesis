0b0d8a0f8257cd7a0f266cb163f5b6cff40c722a
Christos Karamolegkos <xristoskaramo@gmail.com>
2021-12-16 17:11:29 +0200

    Localize table

--- app/Views/table.php
+++ app/Views/table.php
@@ -31,6 +31,37 @@ if (isset($columns, $apiTarget)): ?>
                     ajaxResponse: function (url, params, response) {
                         return response.data;
                     },
+                    <?php if (session()->get('locale') === 'el') : ?>
+                    locale:true,
+                    langs:{
+                        "el":{
+                            "groups":{
+                                "item":"αντικείμενο",
+                                "items":"αντικείμενα",
+                            },
+                            "data":{
+                                "loading":"Φόρτωση",
+                                "error":"Σφάλμα",
+                            },
+                            "pagination":{
+                                "page_size":"Μέγεθος Σελίδας",
+                                "page_title":"Εμφάνιση Σελίδας",
+                                "first":"Πρώτο",
+                                "first_title":"Πρώτη Σελίδα",
+                                "last":"Τελευταίο",
+                                "last_title":"Τελευταία Σελίδας",
+                                "prev":"Προηγούμενο",
+                                "prev_title":"Προηγούμενη Σελίδα",
+                                "next":"Επόμενο",
+                                "next_title":"Επόμενη Σελίδα",
+                                "all":"Όλα",
+                            },
+                            "headerFilters":{
+                                "default":"φίλτρο στήλης...",
+                            }
+                        },
+                    },
+                    <?php endif; ?>
                 });
 
             </script>
