b112a52953ab98641ad3f36a2f8a7a7d4f72ecf7
Christos Karamolegkos <xristoskaramo@gmail.com>
2021-12-20 01:36:24 +0200

    Create database migrations and seeder
    Create the required tables using CodeIgniter 's database migration feature.
    
    Seed the os_image_archs table with default values.

---
+++ app/Database/Migrations/2021-12-19-172100_OsImages.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class OsImages extends Migration
+{
+    public function up()
+    {
+        $this->db->disableForeignKeyChecks();
+
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 10,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '30',
+                'null'       => false,
+            ],
+            'arch' => [
+                'type'       => 'INT',
+                'constraint' => 10,
+                'unsigned'   => true,
+            ],
+            'ipxe_entry' => [
+                'type' => 'TEXT',
+                'null' => false,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('arch', 'os_image_archs', 'id', 'CASCADE', 'RESTRICT');
+        $this->forge->createTable('os_images');
+
+        $this->db->enableForeignKeyChecks();
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('os_images');
+    }
+}
