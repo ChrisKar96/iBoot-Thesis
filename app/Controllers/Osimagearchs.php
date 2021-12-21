e63b38f58eb0cf89462a8854d140e9c2baca9ed1
Christos Karamolegkos <xristoskaramo@gmail.com>
2021-12-21 19:44:58 +0200

    Make Os Image Archs editable and validate input

--- app/Controllers/Osimagearchs.php
+++ app/Controllers/Osimagearchs.php
@@ -12,8 +12,8 @@ class Osimagearchs extends BaseController
                 'title'     => lang('Text.os_image_archs'),
                 'tabulator' => true,
                 'apiTarget' => base_url('/api/osimagearch'),
-                'columns'   => '{title:"' . lang('Text.name') . '", field:"name", sorter:"string"},
-                                {title:"' . lang('Text.description') . '", field:"description", sorter:"string"},',
+                'columns'   => '{title:"' . lang('Text.name') . '", field:"name", sorter:"string", editor:true, validator:["required", "maxLength:20"]},
+                                {title:"' . lang('Text.description') . '", field:"description", sorter:"string", editor:true, editorParams:{allowEmpty:true}, validator:["unique", "maxLength:50"]},',
                 'JS_bef_tb' => '',
             ]
         );
