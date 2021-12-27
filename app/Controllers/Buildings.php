19e05c9e5d90ab477482df078df1de3d2cce48dc
Christos Karamolegkos <xristoskaramo@gmail.com>
2021-12-27 19:22:50 +0200

    Enable editors and validators in most tables
    Computers and groups remaining to fix the multiple choices

--- app/Controllers/Buildings.php
+++ app/Controllers/Buildings.php
@@ -12,9 +12,9 @@ class Buildings extends BaseController
                 'title'     => lang('Text.buildings'),
                 'tabulator' => true,
                 'apiTarget' => base_url('/api/building'),
-                'columns'   => '{title:"' . lang('Text.building') . '", field:"name", sorter:"string"},
-                                {title:"' . lang('Text.address') . '", field:"address", sorter:"string"},
-                                {title:"' . lang('Text.phone') . '", field:"phone", sorter:"string"},',
+                'columns'   => '{title:"' . lang('Text.building') . '", field:"name", sorter:"string", editor:"input", validator:["required", "maxLength:20"]},
+                                {title:"' . lang('Text.address') . '", field:"address", sorter:"string", editor:"input", validator:["maxLength:50"]},
+                                {title:"' . lang('Text.phone') . '", field:"phone", sorter:"string", editor:"input", validator:["maxLength:15"]},',
                 'JS_bef_tb' => '',
             ]
         );
--- app/Controllers/Osimages.php
+++ app/Controllers/Osimages.php
@@ -12,38 +12,37 @@ class Osimages extends BaseController
                 'title'     => lang('Text.os_images'),
                 'tabulator' => true,
                 'apiTarget' => base_url('/api/osimage'),
-                'columns'   => '{title:"' . lang('Text.os_image') . '", field:"name", sorter:"string", width: 200},
-                                {title:"' . lang('Text.arch') . '", field:"arch", sorter:"number", width: 150,
+                'columns'   => '{title:"' . lang('Text.os_image') . '", field:"name", sorter:"string", width: 200, editor:"input", validator:["required", "maxLength:30"]},
+                                {title:"' . lang('Text.arch') . '", field:"arch", sorter:"number", width: 150, validator:["required", "numeric", "min:0"], editor:"select",
+                                    editorParams:{
+                                        values:arch_names
+                                    },
                                     formatter:function (cell) {
-                                        let value = cell.getValue();
-                                        for(let i = 0; i < archs.length; i++) {
-                                            console.log(archs[i].id);
-                                            if(archs[i].id === value) {
-                                                return archs[i].name;
-                                            }
+                                        if(arch_names[cell.getValue()] !== "undefined") {
+											return arch_names[cell.getValue()];
                                         }
                                         return value;
                                     },
                                     tooltip:function(cell){
-                                        if(typeof cell._cell !== "undefined") {
-                                            let value = cell._cell.getValue();
-                                            for(let i = 0; i < archs.length; i++) {
-                                                if(archs[i].id === value) {
-                                                    return archs[i].description;
-                                                }
-                                            }
+                                        if(typeof cell._cell !== "undefined" && arch_descriptions[cell._cell.getValue()] !== "undefined") {
+                                            return arch_descriptions[cell._cell.getValue()];
                                         }
                                     },
                                 },
-                                {title:"' . lang('Text.ipxe_entry') . '", field:"ipxe_entry", formatter:"textarea"},',
-                'JS_bef_tb' => 'let archs = {};
+                                {title:"' . lang('Text.ipxe_entry') . '", field:"ipxe_entry", formatter:"textarea", editor:"textarea", validator:["required"]},',
+                'JS_bef_tb' => 'let arch_names = {};
+                                let arch_descriptions = {};
 
-                                api_call("' . base_url('/api/osimagearch') . '", "GET").then(function(response) {
-                                    /*for (i = 0; i < response.data.length; ++i) {
-                                        archs[response.data[i].id] = response.data[i].name;
-                                    }*/
-                                    archs = response.data;
-                                });
+								async function getArchs(){
+                                    await api_call("' . base_url('/api/osimagearch') . '", "GET").then(function(response) {
+                                        for (i = 0; i < response.data.length; i++) {
+                                            arch_names[response.data[i].id] = response.data[i].name;
+                                            arch_descriptions[response.data[i].id] = response.data[i].description;
+                                        }
+                                    });
+                                }
+
+                                getArchs();
 				',
             ]
         );
