476f378b98c4fed9a3984383216679a03cf4c0e2
Christos Karamolegkos <xristoskaramo@gmail.com>
2022-04-29 22:00:24 +0300

    Update FontAwesome

--- app/Views/partials/head.php
+++ app/Views/partials/head.php
@@ -12,26 +12,26 @@
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <!-- FontAwesome CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
-          integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ=="
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+          integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
           crossorigin="anonymous" referrerpolicy="no-referrer"/>
 
-    <?php
+	<?php
     if (isset($tabulator) && $tabulator) : ?>
         <!-- Tabulator CSS -->
         <link rel="stylesheet" href="https://unpkg.com/tabulator-tables@5.0.7/dist/css/tabulator.min.css">
         <script type="text/javascript" src="https://unpkg.com/tabulator-tables@5.0.7/dist/js/tabulator.min.js"></script>
-    <?php
+	<?php
     endif; ?>
 
     <!-- Custom CSS -->
     <link rel="stylesheet" href="<?= base_url('/assets/css/style.css'); ?>">
 
-    <?php
+	<?php
     if (isset($title) && ($title === lang('Text.log_in') || $title === lang('Text.sign_up'))) : ?>
         <!-- Log In / Register Form CSS -->
         <link rel="stylesheet" href="<?= base_url('/assets/css/login-clean.css'); ?>">
-    <?php
+	<?php
     endif; ?>
 
     <title>iBoot<?= (isset($title)) ? ' - ' . $title : ''; ?></title>
